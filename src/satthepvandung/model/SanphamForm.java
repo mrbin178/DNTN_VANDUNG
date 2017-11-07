31042200159bec53517eeec54d8f4e762f28fc47
mrbin178 <nguyenduongky@gmail.com>
2017-11-07 18:06:16 +0700

    1
    1

---
+++ src/satthepvandung/model/SanphamForm.java
@@ -0,0 +1,63 @@
+package satthepvandung.model;
+
+public class SanphamForm
+{
+	private String id;
+	private String giaBan;
+	private String giaVon;
+	private String maSanPham;
+	private String tenSanPham;
+	private int trangThai;
+	private String ngayTao;
+	private String ngayUpdate;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getGiaBan() {
+		return giaBan;
+	}
+	public void setGiaBan(String giaBan) {
+		this.giaBan = giaBan;
+	}
+	public String getGiaVon() {
+		return giaVon;
+	}
+	public void setGiaVon(String giaVon) {
+		this.giaVon = giaVon;
+	}
+	public String getMaSanPham() {
+		return maSanPham;
+	}
+	public void setMaSanPham(String maSanPham) {
+		this.maSanPham = maSanPham;
+	}
+	public String getTenSanPham() {
+		return tenSanPham;
+	}
+	public void setTenSanPham(String tenSanPham) {
+		this.tenSanPham = tenSanPham;
+	}
+	public int getTrangThai() {
+		return trangThai;
+	}
+	public void setTrangThai(int trangThai) {
+		this.trangThai = trangThai;
+	}
+	public String getNgayTao() {
+		return ngayTao;
+	}
+	public void setNgayTao(String ngayTao) {
+		this.ngayTao = ngayTao;
+	}
+	public String getNgayUpdate() {
+		return ngayUpdate;
+	}
+	public void setNgayUpdate(String ngayUpdate) {
+		this.ngayUpdate = ngayUpdate;
+	}
+	
+}
